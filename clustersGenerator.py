97d7c73aa26077e115f5ebd8c3c5e4c77b2bf20c
LouaiKB <loukb1998@gmail.com>
2020-11-06 23:41:26 +0100

    blast with more than 2 proteomes done!

--- clustersGenerator.py
+++ clustersGenerator.py
@@ -1,8 +1,21 @@
 # import the BlastProcess Class
 from blastpbio import BlastProcess
+from itertools import combinations 
 
 # store the files into variables
-yersiniaProteom = r'Yersinia_pestis_angola.fasta'
-yersiaProteom = r'protéomes_yersia.fasta'
-yersinaPestisBiovar = r'Yersinia_pestis_biovar_microtus_str_91001.ASM788v1.pep.all.fa'
+yersiniaProteom = 'Yersinia_pestis_angola.fasta'
+yersiaProteom = 'protéomes_yersia.fasta'
+yersinaPestisBiovar = 'Yersinia_pestis_biovar_microtus_str_91001.ASM788v1.pep.all.fa'
 
+# lists of proteomes
+proteomeLists = [yersiniaProteom, yersiaProteom, yersinaPestisBiovar]
+
+# creation of combinations object 
+combinatioObject = combinations(proteomeLists, 2)
+
+# creation of proteom combinations 
+proteomeCombinations = [i for i in combinatioObject]
+
+for i in range(len(proteomeCombinations)):
+    blastinstance = BlastProcess(proteomeCombinations[i][0], proteomeCombinations[i][1], 7)
+    blastinstance.reciprocalBlast()
